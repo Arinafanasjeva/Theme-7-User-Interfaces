eaaeae2b199dd4ae75c5aa81d9d3f5cfd881a36a
Arina Afanasjeva <arin0181@stud.kea.dk>
2021-08-31 14:28:14 +0200

    js for productlist

---
+++ Theme 7 User Interfaces/js/productlist.js
@@ -0,0 +1,57 @@
+const url = "https://kea-alt-del.dk/t7/api/products/";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+  //grab the template
+  const template = document.querySelector("#smallProductTemplate").content;
+
+  //clone it
+  const copy = template.cloneNode(true);
+
+  //change content
+  copy.querySelector(
+    ".subtle"
+  ).textContent = `${product.articletype} | ${product.brandname}`;
+
+  copy.querySelector("h3").textContent = product.productdisplayname;
+
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  if (product.discount) {
+    copy.querySelector("article").classList.add("onSale");
+    copy.querySelector(".discounted p").textContent =
+      "Now DKK " +
+      Math.round(product.price - (product.price * product.discount) / 100) +
+      ",-";
+    copy.querySelector(".procents").textContent = "-" + product.discount + "%";
+    copy.querySelector(".price").textContent =
+      "Prev. DKK " + product.price + ",-";
+  } else {
+    copy.querySelector(".price").textContent = "DKK " + product.price + ",-";
+  }
+
+  copy.querySelector(
+    "img.productImage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  copy.querySelector("img.productImage").alt = product.productdisplayname;
+
+  //grab parent
+  const parent = document.querySelector("main");
+
+  //append
+  parent.appendChild(copy);
+}
