cba8951160f4de233133fe0c2eef7f45c90e3d4f
Arina Afanasjeva <arin0181@stud.kea.dk>
2021-09-01 12:03:02 +0200

    product NEW

--- Theme 7 User Interfaces/js/product.js
+++ Theme 7 User Interfaces/js/product.js
@@ -1,4 +1,6 @@
-const url = "https://kea-alt-del.dk/t7/api/products/2801";
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = "https://kea-alt-del.dk/t7/api/products/" + id;
 
 //fetch the data
 fetch(url)
@@ -16,4 +18,33 @@ function showProduct(product) {
   document.querySelector(
     "img.productimage"
   ).alt = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+
+  // SoldOut
+  if (product.soldout) {
+    document.querySelector(".soldOut span").textContent = "ITEM IS SOLDOUT";
+  }
+  // Gender
+  document.querySelector(".gender span").textContent = product.gender;
+  //Discount and prices
+  if (product.discount) {
+    document.querySelector(".discount").classList.add("onSale");
+    document.querySelector(".newPrice span").textContent =
+      "Now DKK " +
+      Math.round(product.price - (product.price * product.discount) / 100) +
+      ",-";
+    document.querySelector(".discount span").textContent =
+      "-" + product.discount + "%";
+    document.querySelector(".price").textContent =
+      "Prev. DKK " + product.price + ",-";
+  } else {
+    document.querySelector(".price span").textContent = product.price;
+    document.querySelector(".discount").textContent = "";
+  }
+  //Category
+  document.querySelector(".category span").textContent = product.category;
+  //Subcategory
+  document.querySelector(".subcategory span").textContent = product.subcategory;
+  //Production year
+  document.querySelector(".productionyear span").textContent =
+    product.productionyear;
 }
